4b7cc9b2aa7fba6fa71d1442499e14539fd69d4c
sarbal <sarahballouz@gmail.com>
2022-06-07 16:35:48 +1000

    culled/combined

--- R/calc_DE.R
+++ R/calc_DE.R
@@ -124,3 +124,40 @@ calc_DE <- function(counts, groups, method, row_filter = FALSE,
     names(combined_out) = c("degs", "output")
     return( combined_out )
 }
+
+
+
+
+#' Reformat DE output
+#'
+#' @param output DE output
+#' @param method method used
+#' @return \code{combined_out}
+#' @examples
+#'
+#' @import edgeR DESeq2 stats
+#' @export
+#'
+#'
+
+reformat_degs <- function(output, method = "wilcox") {
+
+    if (method == "edgeR") {
+        padj <- p.adjust(output$table$PValue )
+        degs <- data.frame(  mean_cpm = 2^output$table$logCPM ,
+                             log2_fc = output$table$logFC,
+                             pvals = output$table$PValue,
+                             padj = padj )
+    }
+    if (method == "DESeq2") {
+        degs <- data.frame(  mean_cpm = output$baseMean,
+                             log2_fc = output$log2FoldChange,
+                             pvals = output$pvalue,
+                             padj =  output$padj )
+
+    }
+    combined_out <- list(degs, output)
+    names(combined_out) = c("degs", "output")
+    return( combined_out )
+}
+
--- R/gene_set_enrichment.R
+++ R/gene_set_enrichment.R
@@ -45,3 +45,42 @@ gene_set_enrichment <- function(genes, genes.labels, voc){
   return (results)
 
 }
+
+
+
+
+#' Gene set enrichment - AUC version
+#'
+#' @param gene_sets GO matrix or some such
+#' @param gene_rankings an array of numeric values
+#' @return \code{results}
+#' @importFrom EGAD auc_multifunc
+#' @export
+
+
+gene_set_enrichment_aucs <- function(gene_sets, gene_rankings){
+
+  
+  m <- match( rownames(gene_sets), names(gene_rankings) )
+  f.g = !is.na(m)
+  f.r = m[f.g]
+  gene_sets = gene_sets[f.g,]
+  gene_rankings = rank(gene_rankings[f.r])
+
+  gene_set_aucs <- EGAD::auc_multifunc(gene_sets, gene_rankings )
+   names(gene_set_aucs) = colnames(gene_sets)
+
+o = order(gene_rankings)
+nbins = round(length(o)/100)
+
+enrich_mat = sapply(1:length(gene_set_aucs), function(j) (sapply(0:nbins, function(i) sum(gene_sets[o,j][ (1:100)+ i*100 ]  )  )  ) )  
+enrich_mat[is.na(enrich_mat)] = 0 
+enrich_mat = t(enrich_mat)/colSums(enrich_mat)
+#enrich_mat = sapply(1:length(gene_set_aucs), function(j) (sapply(0:nbins, function(i) sum(gene_sets[o,j][ (1:1000)+ i*1000 ]  )  )  ) )  
+heatmap.2(  (enrich_mat), col=colssig, trace="none", density="none", Rowv=F, Colv=F)
+
+  return (gene_set_aucs)
+
+}
+
+
